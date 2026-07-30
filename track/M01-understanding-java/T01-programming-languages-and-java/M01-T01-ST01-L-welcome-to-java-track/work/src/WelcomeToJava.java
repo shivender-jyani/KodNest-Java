8ed0d34aca8e3b1e69df445ffcdd67fd9ecf9500
Shivender Jyani <shivenderjyani330.com>
2026-07-30 13:00:30 +0530

    added 1 more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,9 +1,12 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
-       System.out.println("Welcome to java track");
-       System.out.println("I already know how to buil programming logic");
-       System.out.println("Now i am learning how to express that logic in java");
+        //Write your code here
+        System.out.println("Welcome to java track");
+        System.out.println("I already know how to buil programming logic");
+        System.out.println("Now i am learning how to express that logic in java");
+        System.out.println("Now i know how to use git and github");
 
     }
 }
