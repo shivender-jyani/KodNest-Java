4dd4571ed15cb08ff802e4d1f149996d4805c9b7
Shivender Jyani <shivenderjyani330.com>
2026-08-04 12:27:56 +0530

    num

---
+++ track/M02-introduction-to-java/T01-introduction-to-java-and-basic-programming-constructs/num.java
@@ -0,0 +1,14 @@
+
+public class num {
+
+    public static void main(String[] args) {
+        int num = 25;
+        if (num > 0) {
+            System.out.println("Positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
