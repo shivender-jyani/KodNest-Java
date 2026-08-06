56166453f6cc3f5ff2e0bb5d21862a26f5f89d3c
Shivender Jyani <shivenderjyani330.com>
2026-08-06 12:42:17 +0530

    break

--- track/M02-introduction-to-java/T02-control-flow-and-loops/whileNested.java
+++ track/M02-introduction-to-java/T02-control-flow-and-loops/whileNested.java
@@ -2,15 +2,26 @@
 public class whileNested {
 
     public static void main(String[] args) {
+        // int i = 1;
+        // while (i <= 5) {
+        //     int j = 1;
+        //     while (j <= 5) {
+        //         System.out.println(j);
+        //         j++;
+        //     }
+        //     System.out.println();
+        //     i++;
+        // }
+
         int i = 1;
-        while (i <= 5) {
+        do {
             int j = 1;
-            while (j <= 5) {
+            do {
                 System.out.println(j);
                 j++;
-            }
+            } while (j <= 5);
             System.out.println();
             i++;
-        }
+        } while (i <= 5);
     }
 }
