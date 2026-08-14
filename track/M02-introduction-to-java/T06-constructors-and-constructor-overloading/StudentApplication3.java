ea172291244c00bd6aff75f10e7ab50bbdc98207
Shivender Jyani <shivenderjyani330.com>
2026-08-14 12:35:10 +0530

    StudentAp3

---
+++ track/M02-introduction-to-java/T06-constructors-and-constructor-overloading/StudentApplication3.java
@@ -0,0 +1,42 @@
+
+public class StudentApplication3 {
+
+    public static void main(String[] args) {
+        Student s2 = new Student();
+        s2.display();
+        Student s3 = new Student("Manu");
+        s3.display();
+        Student s1 = new Student("Raja", 18, 5.5);
+        s1.display();
+    }
+}
+
+class Student {
+
+    String name;
+    int age;
+    double height;
+
+    Student() {
+        name = "Anu";
+        age = 19;
+        height = 5.0;
+    }
+
+    Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+
+}
