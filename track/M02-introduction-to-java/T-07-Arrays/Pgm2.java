c745c9617ddf02bfda2f71381e888e1aed653bf2
Shivender Jyani <shivenderjyani330.com>
2026-08-17 11:54:42 +0530

    Pgm2

---
+++ track/M02-introduction-to-java/T-07-Arrays/Pgm2.java
@@ -0,0 +1,14 @@
+
+public class Pgm2 {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+}
