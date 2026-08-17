be961ca3d7d5f75988675fbcf44797805c9be000
Shivender Jyani <shivenderjyani330.com>
2026-08-17 12:37:17 +0530

    Pgm3

---
+++ track/M02-introduction-to-java/T-07-Arrays/Pgm3.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Pgm3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("Enter Array Elements: ");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements are: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        sc.close();
+    }
+}
