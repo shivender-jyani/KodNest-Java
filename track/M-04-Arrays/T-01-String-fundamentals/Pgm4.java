aeb935a5bee9bbe538f0d2aa81dac161812cea83
Shivender Jyani <shivenderjyani330.com>
2026-09-07 17:25:41 +0530

    Pgm4

---
+++ track/M-04-Arrays/T-01-String-fundamentals/Pgm4.java
@@ -0,0 +1,13 @@
+
+
+public class Pgm4 {
+    public static void main(String[] args) {
+        String s3 = new String("Java");
+        String s4 = new String("Java");
+        if(s3.equals(s4)){
+            System.out.println("Strings are equal");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+    }
+}
